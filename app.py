731f00d8815be94ceab402835d6f3c5e144d532a
CJ Castillo <caytejohntims@gmail.com>
2026-02-20 16:27:31 +0800

    Update app.py script

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def getCurrentTime():
     currentTime = time.strptime("%Y-%m-%d %H:%M:%S", time.localtime())
-    return f"The current time {currentTime}"
+    return f"The current time: {currentTime}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=9001)
